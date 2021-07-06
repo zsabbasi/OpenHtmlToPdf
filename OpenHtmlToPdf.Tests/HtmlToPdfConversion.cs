32710dd96952a13b43b2eb756ee8e5b4748ccb99
Zeeshan Abbasi <zeeshan@mparsec.com>
2021-07-06 18:37:40 +0500

    --OpenHtmlToPdf .net core

--- OpenHtmlToPdf.Tests/HtmlToPdfConversion.cs
+++ OpenHtmlToPdf.Tests/HtmlToPdfConversion.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenHtmlToPdf.Tests.Helpers;
+using OpenHtmlToPdf;
 
 namespace OpenHtmlToPdf.Tests
 {
